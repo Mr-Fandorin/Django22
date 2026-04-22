ed82b27dda2ae6727d239c0fb899aadc1926b1ef
Sergey Mosykin <smosyakin@yandex.ru>
2026-04-22 21:57:39 +0300

    fixing models.py/blog and catalog

--- catalog/models.py
+++ catalog/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
     views_counter = models.PositiveIntegerField(
         verbose_name = "Счетчик просмотров",
         help_text = "Укажите количество просмотров",
-        db_default=0,
+        default=0,
     )
 
 
