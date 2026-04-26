f80f2170f8ef9a588a64972c2d93d987cfcac7fb
Sergey Mosykin <smosyakin@yandex.ru>
2026-04-26 19:03:49 +0300

    add admin.py, apps.py, csu.py, forms.py, models.py, tests.py, urls.py, views.py (users)

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=35, blank=True, null=True, verbose_name="Телефон", help_text="Введите номер телефона")
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name="Аватар")
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Страна", help_text="Укажите вашу страну")
+
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
