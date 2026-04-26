cdc48a90b9f3c718ddb5f1b7a95fa57abfe1bd7a
Sergey Mosykin <smosyakin@yandex.ru>
2026-04-26 19:00:27 +0300

    fixing settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from django.conf.global_settings import STATICFILES_DIRS, DEFAULT_AUTO_FIELD
+from django.conf.global_settings import STATICFILES_DIRS, DEFAULT_AUTO_FIELD, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 
 load_dotenv()
 
@@ -15,6 +15,7 @@ DEBUG = True if os.getenv("DEBUG") == "True" else False
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
+    "users",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -23,7 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog",
-    # "users",
 ]
 
 MIDDLEWARE = [
@@ -99,4 +99,18 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "TGDV.LLC@yandex.ru"
+EMAIL_HOST_PASSWORD = "jjvoiskelbilvstq"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
